571480b15e3fa508f3e4f10fe85539e7c9cca748
san1196 <hasansulistyo96@gmail.com>
2021-03-30 22:21:40 +0700

    test api

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,16 @@
+<?php
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\About;
+
+class ApiController extends Controller
+{
+    public function get_all_karyawan()
+    {
+        return response()->json(About::all(), 200);
+    }
+}
+
+?>
